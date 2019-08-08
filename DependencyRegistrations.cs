7bc39acc95fad207095626a89607da88b7b20802
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:27:10 -0700

    Add files via upload

---
+++ DependencyRegistrations.cs
@@ -0,0 +1,40 @@
+using Penguin.DependencyInjection.Abstractions;
+using Penguin.DependencyInjection.ServiceProviders;
+using Penguin.Messaging.Abstractions.Interfaces;
+using Penguin.Messaging.Core;
+using Penguin.Reflection;
+using System;
+using System.Collections.Generic;
+using DependencyEngine = Penguin.DependencyInjection.Engine;
+
+namespace Framework.Cms.Web.Mvc.DependencyInjection
+{
+    /// <summary>
+    /// Contains the interface that automatically registers message handler types as Transient with the Dependency Injector
+    /// </summary>
+    public class DependencyRegistrations : IRegisterDependencies
+    {
+        #region Methods
+
+        /// <summary>
+        /// Registers the dependencies.
+        /// </summary>
+        public void RegisterDependencies()
+        {
+            IEnumerable<Type> MessageHandlers = TypeFactory.GetAllImplementations(typeof(IMessageHandler));
+
+            MessageBus.SubscribeAll(MessageHandlers);
+            foreach (Type messageHandler in MessageHandlers)
+            {
+                //We want to register each message handler with the service provider in case
+                //there are dependencies that need to be resolved in order to use it
+                if (!DependencyEngine.IsRegistered(messageHandler))
+                {
+                    DependencyEngine.Register(messageHandler, messageHandler, typeof(TransientServiceProvider));
+                }
+            }
+        }
+
+        #endregion Methods
+    }
+}
