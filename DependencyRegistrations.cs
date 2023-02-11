0f99e1dc3a74e48c237c7a39e99f57ec747b8ed6
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-11 11:44:57 -0700

    Autocommit
    Autocommit

--- DependencyRegistrations.cs
+++ DependencyRegistrations.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using DependencyEngine = Penguin.DependencyInjection.Engine;
 
-
 namespace Penguin.Messaging.DependencyInjection
 {
     /// <summary>
